13a4ffe4474c8c02f8789ce06bc3ff9449b3ce9b
Artur Chagas <artur_life_2015@hotmail.com>
2025-03-11 00:17:27 -0300

    Aula sobre parametros de uma função

--- FUNDAMENTOS-JAVASCRIPT-I/funcoes/funcoes.js
+++ FUNDAMENTOS-JAVASCRIPT-I/funcoes/funcoes.js
@@ -1,6 +1,53 @@
-function ola(){
-    console.log("Ola, mundo!")
+function dobro(x) {
+    alert("O dobro de " + x + " é " + (x * 2));
+
 }
 
-ola()
-ola()
+// dobro(5);
+// dobro(7);
+// dobro()
+
+function dizerOla(nome = "Visitante") {
+    alert("Ola, " + nome + "!");
+}
+
+// dizerOla("Artur");
+// dizerOla();
+
+function soma(a, b) {
+    alert("A soma de " + a + " + " + b + " = " + (a + b));
+}
+// soma(6, 7);
+
+function criarUsuario(nome, email, senha, tipo = 'admin') {
+    const usuario = {
+        nome,
+        email,
+        senha,
+        tipo
+    }
+    console.log(usuario);
+}
+
+
+function muitosParametros(nome, telefone, endereco, aniversario, email, senha) {
+
+}
+function objetoComoParametro(usuario) {
+    usuario.nome,
+    usuario.email
+
+}
+
+muitosParametros('Artur', '123', 'Rua 1', '01/01/2000', 'email', 'senha');
+
+const dadosDoUsuario = {
+    nome: '',
+    telefone: '',
+    endereco: '',
+    aniversario: '',
+    email: '',
+    senha: ''
+}
+
+objetoComoParametro(dadosDoUsuario);
