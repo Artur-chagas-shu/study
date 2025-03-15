0ab26c702e05715a84c1fc5c3c638270653dce9c
Artur Chagas <artur_life_2015@hotmail.com>
2025-03-15 00:02:59 -0300

    Criando a Lista de opções

--- FUNDAMENTOS-JAVASCRIPT-I/calculadora-geometrica/index.js
+++ FUNDAMENTOS-JAVASCRIPT-I/calculadora-geometrica/index.js
@@ -3,10 +3,10 @@ function areaDoTriangulo(base, altura) {
     return base * altura / 2
 }
 
-function areaDoRetagun (base, altura) {
+function areaDoRetagun(base, altura) {
     return base * altura
 }
-function  areaDoQuadrado(lado){
+function areaDoQuadrado(lado) {
     return lado * lado
 }
 function areaDoTrapezio(baseMaior, baseMenor, altura) {
@@ -16,3 +16,17 @@ function areaDoTrapezio(baseMaior, baseMenor, altura) {
 function areaDoCirculo(raio) {
     return 3.14 * raio * raio
 }
+
+
+do {
+    let opcao = parseFloat(prompt("Escolha uma opção\n" +
+        "1: Triângulo\n" +
+        "2: Retângulo\n" +
+        "3: Quadrado\n" +
+        "4: Trapezio\n" +
+        "5: Circulo\n" +
+        "6: Encerrar\n"))
+
+    switch (opcao) {}
+
+    } while (opcao != 6);
