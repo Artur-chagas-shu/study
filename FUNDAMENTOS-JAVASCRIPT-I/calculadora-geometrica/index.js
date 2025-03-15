aa915ae436509db17aa526b1fd2a5584340dc7c8
Artur Chagas <artur_life_2015@hotmail.com>
2025-03-14 23:58:51 -0300

    Criando as funçoes de calculo das figuras Geometricas]

---
+++ FUNDAMENTOS-JAVASCRIPT-I/calculadora-geometrica/index.js
@@ -0,0 +1,18 @@
+
+function areaDoTriangulo(base, altura) {
+    return base * altura / 2
+}
+
+function areaDoRetagun (base, altura) {
+    return base * altura
+}
+function  areaDoQuadrado(lado){
+    return lado * lado
+}
+function areaDoTrapezio(baseMaior, baseMenor, altura) {
+    return (baseMaior + baseMenor) * altura / 2
+}
+
+function areaDoCirculo(raio) {
+    return 3.14 * raio * raio
+}
