fc435995bc4e5f74dc13f7d55678923e3e8267f8
Artur Chagas <artur_life_2015@hotmail.com>
2025-05-16 23:08:19 -0300

    Exemplo real do uso de uma arrow function

--- FUNDAMENTOS-JAVASCRIPT-IV/arrow-functions/index.js
+++ FUNDAMENTOS-JAVASCRIPT-IV/arrow-functions/index.js
@@ -22,3 +22,13 @@ console.log(`Subtração: ${subtract(2,2)}`)
 const double = n => `O dobro de ${n} é ${n * 2}`
 
 console.log(double(21))
+
+
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
+
+
+const startingWithP = towns.filter(town =>{
+    return town [0] === 'P'
+})
+
+console.log(startingWithP)
