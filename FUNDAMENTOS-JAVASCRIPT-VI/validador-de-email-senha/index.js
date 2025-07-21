1ef68e0c8c3ec1421e6ceeb04d5dcb72b32a6c00
Artur Chagas <artur_life_2015@hotmail.com>
2025-07-21 00:08:05 -0300

    finalizando aplicacao

--- FUNDAMENTOS-JAVASCRIPT-VI/validador-de-email-senha/index.js
+++ FUNDAMENTOS-JAVASCRIPT-VI/validador-de-email-senha/index.js
@@ -6,6 +6,27 @@ function validadeEmail(email){
     }
 }
 
+function validatePassword (password){
+    if (
+        password.length < 8 || 
+        !password.match(/[a-z]/) || 
+        !password.match(/[A-Z]/) ||
+        !password.match(/\d/) ||
+        !password.match(/[^a-zA-Z0-9]/) 
+    ){
+        const err = new Error('Senha invalida !')
+        err.input = 'password'
+        throw err
+    }
+}
+function resetFormStyles(inputs){
+    Object.entries(inputs).forEach(([key, value]) => {
+        value.classList.remove('success', 'error')
+        document.querySelector(`#${key}-error`).textContent = ''
+        
+    })
+}
+
 
 
 const userInputs = {
@@ -13,3 +34,22 @@ const userInputs = {
     email: document.querySelector('#email'),
     password: document.querySelector('#password')
 }
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    resetFormStyles(userInputs)
+    try{
+        userInputs.name.classList.add('success')
+        validadeEmail(userInputs.email.value)
+        userInputs.email.classList.add('success')
+        validatePassword(userInputs.password.value)
+        userInputs.password.classList.add('success')
+
+    }catch(err){
+        userInputs[err.input].classList.add('error')
+        document.querySelector(`#${err.input}-error`).textContent = err.message
+    }
+    
+})
