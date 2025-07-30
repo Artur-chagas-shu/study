82918bcf104fa1188ed52c5ed14e9a4a7d1d12ea
Artur Chagas <artur_life_2015@hotmail.com>
2025-07-29 22:44:17 -0300

    Encadeando promises

---
+++ FUNDAMENTOS-JAVASCRIPT-VI/encadeando-promises/index.js
@@ -0,0 +1,32 @@
+ function checkAge(age) {
+    return new Promise((resolve, reject) => {
+        if (age) {
+            resolve(age > 18)
+        } else {
+            reject(new Error('age is required'))
+        }
+    })
+}
+
+function getAge(birthday) {
+    return new Promise((resolve, reject) => {
+        if (birthday) {
+            const birthYear = new Date(birthday).getFullYear()
+            const currentYear = new Date().getFullYear()
+            resolve(currentYear - birthYear)
+        } else {
+            reject(new Error('birthday is required'))
+        }
+    })
+}
+
+getAge('1999-09-02').then((age)=>{
+    return checkAge(age)
+}).then( (isOver18)=> {
+    if (isOver18) {
+            console.log('You are over 18')
+    } else {
+            console.log('You are not over 18')
+    }
+
+})
